e8a4555ec18693797bdaed41ac103700efc00648
PabloMM19 <pmoleromarin@gmail.com>
2024-02-22 07:21:19 +0100

    finals

--- src/app/components/pacientes/pacientes-edit-unrouted/pacientes-edit-unrouted.component.ts
+++ src/app/components/pacientes/pacientes-edit-unrouted/pacientes-edit-unrouted.component.ts
@@ -2,7 +2,9 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { IPacientes } from 'src/app/model/pacientes.model';
+import { ISeguroMedico } from 'src/app/model/seguromedico.model';
 import { PacientesService } from 'src/app/service/pacientes.service';
+import { SeguroMedicoService } from 'src/app/service/seguroMedico.service';
 
 @Component({
   selector: 'app-pacientes-edit-unrouted',
@@ -12,68 +14,43 @@ import { PacientesService } from 'src/app/service/pacientes.service';
 export class PacientesEditUnroutedComponent implements OnInit {
 
   pacienteForm: FormGroup;
+  segurosMedicos: ISeguroMedico[] = [];
 
-  @Input()
-  set paciente(paciente: IPacientes | undefined) {
-    if (paciente) {
-      this.pacienteForm.patchValue({
-        id: paciente.id,
-        codigo: paciente.codigo,
-        //fnacimiento: paciente.fnacimiento,
-        nombre: paciente.nombre,
-        papellido: paciente.papellido,
-        sapellido: paciente.sapellido,
-        foto: paciente.foto,
-        seguroMedico: paciente.seguroMedico.id
-      });
-    } else {
-      this.pacienteForm.patchValue({
-        id: 0,
-        codigo: '',
-        //fnacimiento: '',
-        nombre: '',
-        papellido: '',
-        sapellido: '',
-        foto: '',
-        seguroMedico: { id: 0, companyia: '', descripcion: ''}
-      });
-    }
-  }
+  @Input() paciente: IPacientes | undefined;
 
-  get paciente(): IPacientes {
-    return this.paciente;
-  }
-
-  constructor(private fb: FormBuilder, private pacienteService: PacientesService, private router: Router) {
+  constructor(private fb: FormBuilder, private pacienteService: PacientesService, private router: Router, private seguroMedicoService: SeguroMedicoService) {
     this.pacienteForm = this.fb.group({
       id: [0, Validators.required],
       codigo: ['', Validators.required],
-      fnacimiento: ['', Validators.required],
       nombre: ['', Validators.required],
       papellido: ['', Validators.required],
       sapellido: ['', Validators.required],
       foto: ['', Validators.required],
-      seguroMedico: this.fb.group({
-        id: [1, Validators.required],
-        companyia: ['', Validators.required],
-        descripcion: ['', Validators.required]
-      })
+      seguroMedico: ['', Validators.required] // Deja el campo seguroMedico vacío inicialmente
     });
   }
 
   ngOnInit() {
-    if (this.paciente) {
-      this.pacienteForm.patchValue({
-        id: this.paciente.id,
-        codigo: this.paciente.codigo,
-        //fnacimiento: this.paciente.fnacimiento,
-        nombre: this.paciente.nombre,
-        papellido: this.paciente.papellido,
-        sapellido: this.paciente.sapellido,
-        foto: this.paciente.foto,
-        seguroMedico: this.paciente.seguroMedico.id
-      });
-    }
+    this.getSegurosMedicos(); // Llama a la función para cargar los seguros médicos
+  }
+
+  getSegurosMedicos() {
+    this.seguroMedicoService.getSeguroMedicoData().subscribe(data => {
+      this.segurosMedicos = data.content; // Almacena la lista de seguros médicos en la variable segurosMedicos
+
+      if (this.paciente) {
+        this.pacienteForm.patchValue({
+          id: this.paciente.id,
+          codigo: this.paciente.codigo,
+          //fnacimiento: this.paciente.fnacimiento,
+          nombre: this.paciente.nombre,
+          papellido: this.paciente.papellido,
+          sapellido: this.paciente.sapellido,
+          foto: this.paciente.foto,
+          seguroMedico: this.paciente.seguroMedico.id.toString() // Asigna el ID del seguro médico como una cadena
+        });
+      }
+    });
   }
 
   onSubmit() {
@@ -89,6 +66,13 @@ export class PacientesEditUnroutedComponent implements OnInit {
       });
     } else {
       console.log('Formulario inválido');
+      // Imprimir los mensajes de error de validación en la consola
+      for (const key in this.pacienteForm.controls) {
+        if (this.pacienteForm.controls[key].invalid) {
+          console.log('Campo', key, 'inválido');
+          console.log('Errores:', this.pacienteForm.controls[key].errors);
+        }
+      }
     }
   }
 
